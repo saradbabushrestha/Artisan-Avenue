13ffe2aca61b7cd28f61ea6df024a7524d967177
Sarad Babu Shrestha <saradbabushrestha@Sarad.local>
2024-11-04 21:02:46 +0545

    finishing touch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+  build: {
+    outDir: "build", // Change 'build' to your desired directory name
+  },
+});
